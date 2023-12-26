49bb8e41d218ee07a1350d7e58c1c96158e0e5f6
ebi-droid <59518223+ebi-droid@users.noreply.github.com>
2023-12-26 12:21:02 +0100

    Update create-compose-service.sh

--- create-compose-service.sh
+++ create-compose-service.sh
@@ -18,11 +18,11 @@ User=root
 Group=docker
 WorkingDirectory=$(pwd)
 # Shutdown container (if running) when unit is started
-ExecStartPre=$(which docker-compose) -f docker-compose.yml down
+ExecStartPre=$(which docker-compose) -f docker-compose.yaml down
 # Start container when unit is started
-ExecStart=$(which docker-compose) -f docker-compose.yml up
+ExecStart=$(which docker-compose) -f docker-compose.yaml up
 # Stop container when unit is stopped
-ExecStop=$(which docker-compose) -f docker-compose.yml down
+ExecStop=$(which docker-compose) -f docker-compose.yaml down
 
 [Install]
 WantedBy=multi-user.target
